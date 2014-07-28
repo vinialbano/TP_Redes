e3a7a2ac1e01c18e6f559a210c13e8cdd331251c
Vinicius Vieira Albano <vinialbano@gmail.com>
2014-07-27 23:15:42 -0300

    Mais métodos de Dados 2

--- src/Abstracoes/Trace.java
+++ src/Abstracoes/Trace.java
@@ -1,6 +1,7 @@
 package Abstracoes;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 
@@ -25,7 +26,7 @@ public class Trace {
         IPs = new HashSet<String>();
         pings = new ArrayList<Double>();
         totalDescartes = 0;
-        
+
     }
 
     public HashSet<String> getIPs() {
@@ -35,19 +36,18 @@ public class Trace {
     public void addLinha(Linha linha) {
         linhas.add(linha);
         IPs.add(linha.getP1().getIP());
-        if (linha.getP1().getIP().compareTo("*")!=0){
+        if (linha.getP1().getIP().compareTo("*") != 0) {
             pings.add(linha.getP1().getPing());
-        }       
+        }
         IPs.add(linha.getP2().getIP());
-         if (linha.getP2().getIP().compareTo("*")!=0){
+        if (linha.getP2().getIP().compareTo("*") != 0) {
             pings.add(linha.getP2().getPing());
-        }        
+        }
         IPs.add(linha.getP3().getIP());
-        if (linha.getP3().getIP().compareTo("*")!=0){
+        if (linha.getP3().getIP().compareTo("*") != 0) {
             pings.add(linha.getP3().getPing());
         }
-        totalDescartes+=linha.getDescartes();
-        
+        totalDescartes += linha.getDescartes();
 
     }
 
@@ -58,17 +58,17 @@ public class Trace {
     public ArrayList<Linha> getLinhas() {
         return linhas;
     }
-    
-    public int getTotalSaltos(){
+
+    public int getTotalSaltos() {
         return linhas.size();
     }
-    
-    public int getTotalDescartes(){
+
+    public int getTotalDescartes() {
         return totalDescartes;
     }
-    
-    public ArrayList<Double> getPings(){
-      return pings;
+
+    public ArrayList<Double> getPings() {
+        return pings;
     }
 
 }
--- src/Estatisticas/Dados.java
+++ src/Estatisticas/Dados.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Estatisticas;
 
 import Abstracoes.Leitor;
 import Abstracoes.Trace;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 
 /**
@@ -17,61 +18,107 @@ import java.util.HashSet;
  * @author Vinicius
  */
 public class Dados {
-    
+
     private HashSet<Trace> traceRoutes;
     private HashSet<String> IPs;
     private int totalSaltos, totalDescartes;
     private ArrayList<Integer> saltos, descartes;
-    
-    public Dados(String path) throws IOException{
-        totalSaltos=0;
-        totalDescartes=0;
+    private ArrayList<Double> pings;
+    private Comparator<Double> comp = new Comparator<Double>() {
+        @Override
+        public int compare(Double o1, Double o2) {
+            if (o1.doubleValue() < o2.doubleValue()) {
+                return -1;
+            } else if (o1.doubleValue() == o2.doubleValue()) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    };
+
+    public Dados(String path) throws IOException {
+        totalSaltos = 0;
+        totalDescartes = 0;
         traceRoutes = Leitor.getTraceRoutes(path);
         saltos = new ArrayList<Integer>();
         descartes = new ArrayList<Integer>();
+        pings = new ArrayList<Double>();
         IPs = new HashSet<String>();
-        for (Trace trace: traceRoutes){
-           IPs.addAll(trace.getIPs());
-           totalSaltos+=trace.getTotalSaltos();
-           saltos.add(trace.getTotalSaltos());
-           totalDescartes+=trace.getTotalDescartes();
-           descartes.add(trace.getTotalDescartes());
+        for (Trace trace : traceRoutes) {
+            IPs.addAll(trace.getIPs());
+            totalSaltos += trace.getTotalSaltos();
+            saltos.add(trace.getTotalSaltos());
+            pings.addAll(trace.getPings());
+            totalDescartes += trace.getTotalDescartes();
+            descartes.add(trace.getTotalDescartes());
         }
         IPs.remove("*");
-        
+
     }
-    
-    public HashSet<String> getIPs(){     
+
+    public HashSet<String> getIPs() {
         return IPs;
     }
-    
-    public int getNumIPsDistintos(){
+
+    public int getNumIPsDistintos() {
         return IPs.size();
     }
-    
-    public int getTotalSaltos(){
+
+    public int getTotalSaltos() {
         return totalSaltos;
     }
-    
-    public ArrayList<Integer> getSaltosPorTrace(){
-        return saltos;        
+
+    public ArrayList<Integer> getSaltosPorTrace() {
+        return saltos;
     }
-    
-    public int getTotalDescartes(){
+
+    public int getTotalDescartes() {
         return totalDescartes;
     }
-    
-    public ArrayList<Integer> getDescartesPorTrace(){
+
+    public ArrayList<Integer> getDescartesPorTrace() {
         return descartes;
     }
-    
-    public ArrayList<Integer> getDescartesPorLinha(int linha){
+
+    public ArrayList<Integer> getDescartesPorLinha(int linha) {
         ArrayList<Integer> descartes = new ArrayList<Integer>();
-        for (Trace trace: traceRoutes){
+        for (Trace trace : traceRoutes) {
             descartes.add(trace.getLinhas().get(linha).getDescartes());
-        }       
+        }
         return descartes;
     }
+
+    public double getMenorPingTotal() {
+        Collections.sort(pings, comp);
+        return pings.get(0);
+    }
+
+    public double getMaiorPingTotal() {
+        Collections.sort(pings, comp);
+        return pings.get(pings.size() - 1);
+    }
+
+    public ArrayList<Double> getMaiorPingPorTrace() {
+        ArrayList<Double> pings = new ArrayList<Double>();
+        ArrayList<Double> pings2;
+        for (Trace trace : traceRoutes) {
+            pings2 = trace.getPings();
+            Collections.sort(pings2, comp);
+            pings.add(pings2.get(pings2.size()-1));
+        }
+        return pings;
+    }
     
-    
+    public ArrayList<Double> getMenorPingPorTrace() {
+        ArrayList<Double> pings = new ArrayList<Double>();
+        ArrayList<Double> pings2;
+        for (Trace trace : traceRoutes) {
+            pings2 = trace.getPings();
+            Collections.sort(pings2, comp);
+            pings.add(pings2.get(0));
+        }
+        return pings;
+    }
+
 }
